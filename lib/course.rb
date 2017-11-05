23bda3abb4115194f3ca5813fa380e18b61c51fa
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-11-05 21:59:30 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,10 +1,10 @@
 class Course
   attr_accessor :title, :schedule, :description
-  
+
   def initialize(title, schedule, description)
     @title = title
-    @schedule = schedule 
+    @schedule = schedule
     @description = description
   end
-    
+
 end
